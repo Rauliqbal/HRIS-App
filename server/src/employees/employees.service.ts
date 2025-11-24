74432d42031824481857d37f659f1c55a934716c
Rauliqbal <muhamadrauliqbal.13@gmail.com>
2025-11-24 17:03:11 +0700

    wip

--- server/src/employees/employees.service.ts
+++ server/src/employees/employees.service.ts
@@ -1,4 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import argon from 'argon2';
+import { Employees } from './entities/employees.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class EmployeesService {}
+export class EmployeesService {
+  constructor(
+    @InjectRepository(Employees)
+    private repo: Repository<Employees>,
+  ) {}
+
+  async create(email: string, password: string) {
+    const hashed = await argon.hash(password);
+
+    const user = this.repo.create({ email, password: hashed });
+
+    return this.repo.save(user);
+  }
+
+  async findByEmail(email: string) {
+    return this.repo.findOne({ where: { email } });
+  }
+}
