98973ce410d982ca9d7520db2c1eb0c122250568
Rauliqbal <muhamadrauliqbal.13@gmail.com>
2025-11-25 17:05:16 +0700

    wip

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { EmployeesModule } from './employees/employees.module';
-import { RolesModule } from './roles/roles.module';
-
+import { AuthController } from './auth/auth.controller';
+import { AuthModule } from './auth/auth.module';
+import { AuthService } from './auth/auth.service';
 @Module({
-  imports: [EmployeesModule, RolesModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [AuthModule],
+  controllers: [AppController, AuthController],
+  providers: [AppService, AuthService],
 })
 export class AppModule {}
---
+++ server/src/auth/dto/register-auth.dto.ts
@@ -0,0 +1,20 @@
+
+import { IsString } from 'class-validator';
+
+export class RegisterDto {
+
+  @IsString()
+  full_name: string
+
+  @IsString()
+  email:string
+
+  @IsString()
+  password:string
+  
+  @IsString()
+  phone:string
+
+  @IsString()
+  status:string
+}
